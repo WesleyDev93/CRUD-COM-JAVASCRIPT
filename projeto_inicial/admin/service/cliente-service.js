25585e15fcec354d1317cb199fa09469abf3a7ef
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-13 10:25:35 -0300

    usando PUT para atualização do editar e logica manipulando o DOM

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,100 +1,61 @@
- const listaClientes = () => {
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
 
-  return fetch (`http://localhost:3000/profile`)
-  .then (resposta => {
-     return resposta.json ()
-  });
+            return resposta.json()
 
- };
+        })
+}
 
-    
-
-const criaCliente = (nome, email) => { 
+const criaCliente = (nome, email) => {
     return fetch(`http://localhost:3000/profile`, {
-        method: 'POST', 
+        method: 'POST',
         headers: {
-            'Content-Type' : 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify({
             nome: nome,
             email: email
         })
     })
-    .then( resposta => {
-        return resposta.body
-    })
+        .then(resposta => {
+            return resposta.body
+        })
 }
-
-
-
-
 
 const removeCliente = (id) => {
     return fetch(`http://localhost:3000/profile/${id}`, {
         method: 'DELETE'
-
     })
 }
 
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+        .then(resposta => {
+            return resposta.json()
+        })
+}
+
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
+    })
+        .then(resposta => {
+            return resposta.json()
+        })
+}
 
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente 
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
-
-
-
-
-
-
-
-
-
-
-
-/*  const listaClientes = () => {
-
-    const promise = new Promise((resolve, reject) => {
-
-        
-    const http = new XMLHttpRequest ()
-
-    http.open ('GET', 'http://localhost:3000/profile');
-
-    http.send ();
-         
-        http.onload = () => {
-    
-            if (http.status > 400 ) {
-                reject (JSON.parse(http.response));
-            } else {
-                resolve (JSON.parse(http.response));
-            }
-        
-        
-        
-        
-        }
-
-
-    })
-
-
-
-   return promise
-
- } */
-
-
-
-
-
-
-
-
-
-
-        
- 
-
