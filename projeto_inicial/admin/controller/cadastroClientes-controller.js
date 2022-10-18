644158410785c5d599052a6a798c4e27c8ad733e
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-18 09:48:51 -0300

    implementando erros com cath

--- projeto_inicial/admin/controller/cadastroClientes-controller.js
+++ projeto_inicial/admin/controller/cadastroClientes-controller.js
@@ -1,24 +1,19 @@
-import {clienteService} from "../service/cliente-service.js";
+import { clienteService } from '../service/cliente-service.js'
 
-const formulario = document.querySelector ('[data-form]');
-
-
-
-//const cadastrar = document.querySelector ('.botao');
+const formulario = document.querySelector('[data-form]')
 
 
-formulario.addEventListener ('submit', (evento) => {
-     evento.preventDefault ()
+formulario.addEventListener('submit', async (evento) => {
+  evento.preventDefault()
+  try {
+    const nome = evento.target.querySelector('[data-nome]').value
+    const email = evento.target.querySelector('[data-email]').value
 
-     const nome = evento.target.querySelector ('[data-nome]').value
-     const email = evento.target.querySelector ('[data-email]').value
-
-     clienteService.criaCliente (nome,email)
-     .then ( () => {
-        window.location.href = '../telas/cadastro_concluido.html'
-     }) 
-
-     
+    await clienteService.criaCliente(nome, email)
+    window.location.href = '../telas/cadastro_concluido.html'
+  }
+  catch (erro) {
+    console.log(erro)
+    window.location.href = "../telas/erro.html"
+  }
 })
-
-
